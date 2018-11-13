f53951c95433f386a1946cc62d343a566d8be988
huos3203 <724987481@qq.com>
2018-11-13 17:51:11 +0800

    Initial Commit

---
+++ hsg.lib.Utils/hsg_lib_Utils.h
@@ -0,0 +1,19 @@
+//
+//  hsg_lib_Utils.h
+//  hsg.lib.Utils
+//
+//  Created by admin on 2018/11/13.
+//  Copyright © 2018年 clcw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hsg_lib_Utils.
+FOUNDATION_EXPORT double hsg_lib_UtilsVersionNumber;
+
+//! Project version string for hsg_lib_Utils.
+FOUNDATION_EXPORT const unsigned char hsg_lib_UtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hsg_lib_Utils/PublicHeader.h>
+
+
